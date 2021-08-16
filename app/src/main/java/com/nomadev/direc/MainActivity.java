fac2af38f0039c5e68f95bc34d85a3b387fbcd52
atharianr <atharianrahmadani@gmail.com>
2021-08-16 09:51:52 +0700

    first commit

---
+++ app/src/main/java/com/nomadev/direc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nomadev.direc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

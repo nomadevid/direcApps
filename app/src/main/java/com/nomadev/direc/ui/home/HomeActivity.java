bf6c8f738bc13f58966e8f8514dae62bb9f0bfa0
atharianr <atharianrahmadani@gmail.com>
2021-08-16 20:59:07 +0700

    beberapa item Home

---
+++ app/src/main/java/com/nomadev/direc/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.nomadev.direc.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nomadev.direc.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}

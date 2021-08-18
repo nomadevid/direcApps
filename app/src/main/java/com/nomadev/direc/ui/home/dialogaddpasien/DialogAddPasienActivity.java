854fcddc3c304aaf0bb1a7adf6d5ce1461129c43
atharianr <atharianrahmadani@gmail.com>
2021-08-18 20:19:05 +0700

    home layout

---
+++ app/src/main/java/com/nomadev/direc/ui/home/dialogaddpasien/DialogAddPasienActivity.java
@@ -0,0 +1,16 @@
+package com.nomadev.direc.ui.home.dialogaddpasien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nomadev.direc.R;
+
+public class DialogAddPasienActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_add_pasien);
+    }
+}

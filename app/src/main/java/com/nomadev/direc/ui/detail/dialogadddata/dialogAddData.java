7cbbd0351a27e2a2b9effeef4ee638ce5ec03798
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2021-08-20 16:08:34 +0700

    Dialog Add Pasien + Add Dialog Data

---
+++ app/src/main/java/com/nomadev/direc/ui/detail/dialogadddata/dialogAddData.java
@@ -0,0 +1,14 @@
+package com.nomadev.direc.ui.detail.dialogadddata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dialogAddData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_add_data);
+    }
+}

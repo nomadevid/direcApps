3e3eae4aa206b162b070d40163bdd9756c223675
atharianr <atharianrahmadani@gmail.com>
2021-09-17 21:02:11 +0700

    rv_image gone after add hasilperiksa, layout and func lapor_error, login activity email, credential security for MailAPI email and password

--- app/src/main/java/com/nomadev/direc/ui/login/LoginActivity.java
+++ app/src/main/java/com/nomadev/direc/ui/login/LoginActivity.java
@@ -1,29 +1,20 @@
 package com.nomadev.direc.ui.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.nomadev.direc.R;
-import com.nomadev.direc.ui.CheckCurrentUser;
 import com.nomadev.direc.ui.home.HomeActivity;
-
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     TextInputLayout editTextUsername, editTextPassword;
@@ -37,72 +28,72 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.Theme_DirecApps);
         setContentView(R.layout.activity_login);
 
-        editTextUsername=findViewById(R.id.editTextTextUsername);
-        editTextPassword=findViewById(R.id.editTextPassword);
-        buttonMasuk=findViewById(R.id.buttonMasuk);
-        progressBar=findViewById(R.id.progressBar);
-        firebaseAuth=FirebaseAuth.getInstance();
+        editTextUsername = findViewById(R.id.editTextTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonMasuk = findViewById(R.id.buttonMasuk);
+        progressBar = findViewById(R.id.progressBar);
+        firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-        if(firebaseUser != null){
+        if (firebaseUser != null) {
             Intent i = new Intent(LoginActivity.this, HomeActivity.class);
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
-         }
+        }
 
+        buttonMasuk.setOnClickListener(view -> {
+            String username = "";
+            String password = "";
 
-        buttonMasuk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username;
-                username = "admin@gmail.com";
-                String password= Objects.requireNonNull(editTextPassword.getEditText().getText()).toString().trim();
+            if (editTextUsername.getEditText() != null) {
+                username = editTextUsername.getEditText().getText().toString().trim();
+            }
 
-                if(TextUtils.isEmpty(username)){
-                    editTextUsername.setError("Masukkan Username anda");
-                    return;
-                }
-                else {
-                    editTextUsername.setError((null));
-                }
+            if (editTextPassword.getEditText() != null) {
+                password = editTextPassword.getEditText().getText().toString().trim();
+            }
 
-                if(TextUtils.isEmpty(password)){
-                    editTextPassword.setError("Masukkan Kata sandi anda");
-                    return;
-                }
-                else {
-                    editTextPassword.setError((null));
-                }
+            if (TextUtils.isEmpty(username)) {
+                editTextUsername.setError("Masukkan Username anda");
+                return;
+            } else {
+                editTextUsername.setError(null);
+            }
 
-                if(password.length()<6){
-                    editTextPassword.setError("Kata sandi harus lebih dari 5 karakter");
-                    return;
-                }
-                else {
-                    editTextPassword.setError((null));
-                }
+            if (TextUtils.isEmpty(password)) {
+                editTextPassword.setError("Masukkan Kata sandi anda");
+                return;
+            } else {
+                editTextPassword.setError(null);
+            }
 
-                progressBar.setVisibility(view.VISIBLE);
+            if (password.length() < 6) {
+                editTextPassword.setError("Kata sandi harus lebih dari 5 karakter");
+                return;
+            } else {
+                editTextPassword.setError(null);
+            }
 
-                //autentikasi firebase
-                firebaseAuth.signInWithEmailAndPassword(username,password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"Berhasil Masuk",Toast.LENGTH_SHORT).show();
+            String email = username + "@gmail.com";
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            //autentikasi firebase
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Berhasil Masuk", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             startActivity(intent);
                             finish();
+                        } else {
+                            if (task.getException() != null) {
+                                Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                            progressBar.setVisibility(View.GONE);
                         }
-                        else {
-                            Toast.makeText(LoginActivity.this,"Error ! "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(view.GONE);
-                        }
-                    }
-                });
-            }
+                    });
         });
 
     }

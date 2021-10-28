175b5c3c979ffaacb0bedee7304dbce37ee5a0e9
atharianr <atharianrahmadani@gmail.com>
2021-10-28 15:00:10 +0700

    fixing export on api 30+

--- app/src/main/java/com/nomadev/direc/ui/detail/DetailActivity.java
+++ app/src/main/java/com/nomadev/direc/ui/detail/DetailActivity.java
@@ -255,11 +255,12 @@ public class DetailActivity extends AppCompatActivity implements
 
         String folderName = "Direc";
         String fileName = folderName + "_" + nama + "_" + System.currentTimeMillis() + ".xls";
-        String path = Environment.getExternalStorageDirectory() + File.separator + folderName + File.separator + fileName;
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + File.separator + folderName + File.separator + fileName;
 
-        File file = new File(Environment.getExternalStorageDirectory() + File.separator + folderName);
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + File.separator + folderName);
         if (!file.exists()) {
             file.mkdirs();
+            Toast.makeText(getApplicationContext(), "FOLDER CREATED", Toast.LENGTH_LONG).show();
         }
 
         FileOutputStream outputStream = null;
